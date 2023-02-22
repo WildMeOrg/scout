8caf8a1bfea0d14e996927b42eb90683fc2b40ed
Jon Van Oast <jon@wildme.org>
2023-02-22 14:41:27 -0700

    initial work

---
+++ api/controllers/task-images-preview.js
@@ -0,0 +1,79 @@
+module.exports = {
+
+
+  friendlyName: 'Task Images Preview',
+
+
+  description: '',
+
+
+  inputs: {
+    taskid : {
+      type : 'string',
+      description : 'id of task'
+    }
+  },
+
+
+  exits: {
+    success: {
+      responseType: 'view',
+      viewTemplatePath: 'pages/task-images-preview'
+    },
+    notLoggedIn: {
+      responseType: 'redirect',
+      description: 'The user is not logged in, so they cannot access this page'
+    },
+    forbidden: {
+      responseType: 'redirect',
+      description: 'The user is not allowed to access this page'
+    },
+    notFound: {
+      responseType: 'redirect',
+      description: 'The specified task could not be found'
+    },
+    congrats: {
+      responseType: 'redirect',
+      description: 'The user is done ground-truthing'
+    },
+    removeBack : {
+      responseType: 'redirect'
+    }
+  },
+
+
+  fn: async function (inputs) {
+
+    // Redirect if not logged in
+    if(!this.req.session.userId) throw { notLoggedIn: '/'};
+
+    // Redirect if not admin
+    if(this.req.session.userRole !== 'admin') throw { forbidden: '/403'};
+
+    // Check that task exists, or return 404
+    let tasks = await Tasks.find({where : {id : inputs.taskid }, limit : 1});
+    if(!tasks.length){
+      throw { notFound: '/404'};
+    }
+    let taskData = tasks[0];
+
+  let images = await Images.find({
+    where : {
+      taskIds : [taskData.id]
+    }
+  });
+
+    let serverData = {
+      taskData: taskData,
+      images: images
+    };
+    let clientData = {
+      taskId : inputs.taskid
+    };
+    let data = await sails.helpers.viewData(this.req,serverData,clientData);
+    return data;
+
+  }
+
+
+};
