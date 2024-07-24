c49f33c539f12fb94eaa5f92b99d20e7877e2117
tsubramanian <tamilselvan@gmail.com>
2024-07-24 19:37:23 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+export DOCKER_BUILDKIT=1
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+# Change to the script's root directory location
+cd ${ROOT_LOC}
+
+# Build the images in dependence order
+while [ $# -ge 1 ]; do
+    if [ "$1" == "scout" ]; then
+        docker build \
+            --compress \
+            -t wildme/scout:latest \
+            --no-cache
+    else
+        echo "Image $1 not found"
+        exit 1
+    fi
+    shift
+done
